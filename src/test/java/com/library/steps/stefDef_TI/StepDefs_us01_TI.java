d87e442043023099a7e67387d9fa700464b4ca58
Turhan Ismailov <turhan.ismailov@gmail.com>
2024-04-07 12:57:48 +0100

    git

---
+++ src/test/java/com/library/steps/stefDef_TI/StepDefs_us01_TI.java
@@ -0,0 +1,15 @@
+package com.library.steps.stefDef_TI;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefs_us01_TI {
+    @When("Execute query to get all IDs from users TI")
+    public void execute_query_to_get_all_i_ds_from_users_ti() {
+
+    }
+    @Then("verify all users has unique ID TI")
+    public void verify_all_users_has_unique_id_ti() {
+
+    }
+}
