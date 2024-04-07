b726cbad1f9c07e4eadf2752c8a24a536b157fc6
elifkanlioglu <elifkanlioglu@gmail.com>
2024-04-07 15:20:25 +0100

    update

---
+++ src/test/java/com/library/steps/stepDef_EK/StepDefs_us03_EK.java
@@ -0,0 +1,19 @@
+package com.library.steps.stepDef_EK;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefs_us03_EK {
+    @When("the user navigates to {string} page EK")
+    public void the_user_navigates_to_page_ek(String string) {
+
+    }
+    @When("the user clicks book categories EK")
+    public void the_user_clicks_book_categories_ek() {
+
+    }
+    @Then("verify book categories must match book_categories table from db EK")
+    public void verify_book_categories_must_match_book_categories_table_from_db_ek() {
+
+    }
+}
