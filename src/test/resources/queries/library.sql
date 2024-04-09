18e9f66be4dc34d22d7a46d96c338883e80b1358
musta <mustafaoguzhan93@gmail.com>
2024-04-09 21:41:04 +0100

    Updated

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -1,12 +1,18 @@
 select * from users;
 
 -- US01-1
-   -- OPT 1
-    select count(id) from users;           -- 138  -- ACTUAL
-    select  count(distinct id) from users; -- 138  -- EXPECTED
+-- OPT 1
+select count(id) from users;           -- 138  -- ACTUAL
+select  count(distinct id) from users; -- 138  -- EXPECTED
 
-   -- OPT 2
-    select id from users;
-    -- getAllColumnAsList --> List --> size  --> EXPECTED
-    -- getAllColumnAsList --> Set  --> size  --> EXPECTED
+-- OPT 2
+select id from users;
+-- getAllColumnAsList --> List --> size  --> EXPECTED
+-- getAllColumnAsList --> Set  --> size  --> EXPECTED
 
+select * from users;
+
+
+select count(*) from book_borrow where is_returned=0;
+
+select name from book_categories;
